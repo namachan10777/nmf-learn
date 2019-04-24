6590c6c501f4305f2e9a8a2c22f227bba5a6e292
namachan10777 <namachan10777@gmail.com>
2019-04-24 14:43:00 +0900

    片方だけ分離できた(謎)

--- ICA/ica.py
+++ ICA/ica.py
@@ -10,24 +10,34 @@ def laplace(phi, mu, x):
     return m.exp(-abs(x-mu)/phi) / phi / 2
 
 def score(x):
-    return laplace(0.5, 0.0, x)
+    return laplace(1.0, 0.0, x)
+
+def normalize(ipt):
+    r1 = np.max(ipt[0])
+    r2 = np.max(ipt[1])
+    return np.array([ipt[0] / r1, ipt[1] / r2])
 
 if __name__ == '__main__':
+    I = np.array([[1.0, 0.0], [0.0, 1.0]])
     wav1, rate1 = sf.read('mix1.wav')
     wav2, rate2 = sf.read('mix2.wav')
+    ipt = np.array([wav1, wav2])
+    step = 0.2
     w = np.array([[rnd.random() - 0.5, rnd.random() - 0.5], [rnd.random() - 0.5, rnd.random() - 0.5]])
-    step = 0.1
-    for i in range(0, 10):
+
+    for i in range(0, 20):
+        # 正規化
+        ipt = normalize(ipt)
         entropy = np.array([[0., 0.], [0., 0.]])
         for t in range(0, len(wav1)):
             phi = np.array([
-                [score(wav1[t]) * wav1[t], score(wav1[t]) * wav2[t]],
-                [score(wav2[t]) * wav1[t], score(wav2[t]) * wav2[t]]])
+                [score(ipt[0][t]) * ipt[0][t], score(ipt[0][t]) * ipt[1][t]],
+                [score(ipt[1][t]) * ipt[0][t], score(ipt[1][t]) * ipt[1][t]]])
             entropy = np.add(entropy, phi)
-        np.divide(entropy, t)
-    w = w + np.dot((np.array([[1, 0], [0, 1]]) - entropy) * step, w)
+        entropy = np.divide(entropy, len(wav1))
+        w = w - step * np.dot(I - entropy.T, w.T)
+        ipt = np.dot(w, ipt)
 
-    input_matrix = np.array([wav1, wav2])
-    out = np.dot(w, input_matrix)
-    sf.write('ica1.wav', out[0], rate1)
-    sf.write('ica2.wav', out[1], rate2)
+    ipt = normalize(ipt)
+    sf.write('ica1.wav', ipt[0], rate1)
+    sf.write('ica2.wav', ipt[1], rate2)
