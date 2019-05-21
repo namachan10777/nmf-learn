f077fa8aecf35d5b767c2c6e45ebf35066a61e9e
namachan10777 <namachan10777@gmail.com>
2019-05-22 01:42:11 +0900

    impl main in mix.py

--- ICA/mix.py
+++ ICA/mix.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
 # 2019 Nakano Masaki
 
+import sys
 import soundfile as sf
 import numpy as np
+
+if __name__ == '__main__':
+    n = len(sys.args)
+    mix = np.random.rand(n, n) * 2 - 1
+    _, samplerate = sf.read(sys.args[1])
+    wavs = []
+    for f in sys.args:
+        wav, rate = sf.read(f)
+        assert rate, 'contradictory samplerate'
+        wavs.append(wav)
+
+    mixed = mix * np.array(wavs)
+
 
 data1, samplrate1 =  sf.read('s1.wav')
 data2, samplrate2 =  sf.read('s2.wav')
